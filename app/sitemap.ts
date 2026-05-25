51dd2a7dadc6ea0fefac7bb4b2433ba2860333f5
kali <kali@kali-xps13>
2026-05-25 22:32:56 +0200

    Update sitemap URLs to freedombonsai.com

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://freedombonsai.vercel.app",
+      url: "https://freedombonsai.com",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://freedombonsai.vercel.app/dona",
+      url: "https://freedombonsai.com/dona",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://freedombonsai.vercel.app/liberati",
+      url: "https://freedombonsai.com/liberati",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.6,
